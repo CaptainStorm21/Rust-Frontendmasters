6c572f4c7be428c437db1fcc0cc84ded29f35c65
Captain Storm <CaptainStorm21@users.noreply.github.com>
2022-02-03 18:10:49 -0800

    Create 3-type-annotation.rs

---
+++ 3-type-annotation.rs
@@ -0,0 +1,19 @@
+
+
+fn main(){
+    let answer = multiply_both(2.3, 2.3);
+    println!("2.3 * 2.3 = {}", answer);
+}
+
+fn multiply_both(x:f64, y:f64) -> f64{
+    return x*y;
+}
+
+// fn main() {
+
+//     let x: f64 = 1.5;
+//     let y = 3.1;
+    
+//   println!("x times y is {}", x * y);
+
+// }    
