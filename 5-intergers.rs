cd43ef3632a7483d3cdb5761e693ae65f6a0d0b1
Captain Storm <CaptainStorm21@users.noreply.github.com>
2022-02-03 18:20:15 -0800

    Create 5-intergers.rs

---
+++ 5-intergers.rs
@@ -0,0 +1,13 @@
+
+// let x: f64 = 10.0/3.0;
+// let y: f32 = 10.0/3.0;
+
+// let nine = 9;
+
+// let one_thousand = 1_1000;
+// println!({},one_thousand);
+
+// let exactly_three = 10 / 3; 
+// println!({},exactly_three);
+
+let  this_will_panic = 6 / 0; //kaboom!
