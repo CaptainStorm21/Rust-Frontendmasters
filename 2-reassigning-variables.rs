14fcb49753bae8c03880ae492e60147cf64d8d4b
Captain Storm <CaptainStorm21@users.noreply.github.com>
2022-02-03 17:45:01 -0800

    Create 2-reassigning-variables.rs

---
+++ 2-reassigning-variables.rs
@@ -0,0 +1,11 @@
+fn main() {
+// cannot be  reassigned or mutated
+    // let x = 2.1;
+    //     x = 10.5;
+    
+    let mut z = 23.4;
+      println!("{}", z);
+        z = 90.8;
+     println!("{}", z);
+}    
+
