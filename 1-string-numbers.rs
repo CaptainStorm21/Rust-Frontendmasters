a634af67158968679d4c833e7b63f9375cb61538
Captain Storm <CaptainStorm21@users.noreply.github.com>
2022-02-03 17:39:45 -0800

    Create 1-string-numbers.rs

---
+++ 1-string-numbers.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, world!");
+    
+    let greeting = "Helo!";
+    let subject = "World";
+    
+    println!("{}, {}", greeting, subject);
+    
+    let alice = "Alice";
+    let goodnight = format!("Good, {}", alice);
+    println!("output is {}",goodnight);
+    
+    let x = 3.90;
+    let y = 2.40;
+    println!("{}",  x*y);
+}    
+
